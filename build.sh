3ad9811186db63b441033d8fa8edc33c99579085
Chris Berthiaume <chrisbee@uw.edu>
2020-02-20 16:30:23 -0800

    Add version to build folder/zip file

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Build cruisemic command-line tool for 64-bit MacOS and Linux
 
+VERSION=$(git describe --long --dirty)
+
 [[ -d cruisemic.darwin-amd64 ]] && rm -rf cruisemic.darwin-amd64
 [[ -d cruisemic.linux-amd64 ]] && rm -rf cruisemic.linux-amd64
-GOOS=darwin GOARCH=amd64 go build -o cruisemic.darwin-amd64/cruisemic cmd/cruisemic/main.go || exit 1
-GOOS=linux GOARCH=amd64 go build -o cruisemic.linux-amd64/cruisemic cmd/cruisemic/main.go || exit 1
-zip -q -r cruisemic.darwin-amd64.zip cruisemic.darwin-amd64 || exit 1
-zip -q -r cruisemic.linux-amd64.zip cruisemic.linux-amd64 || exit 1
+GOOS=darwin GOARCH=amd64 go build -o cruisemic.${VERSION}.darwin-amd64/cruisemic cmd/cruisemic/main.go || exit 1
+GOOS=linux GOARCH=amd64 go build -o cruisemic.${VERSION}.linux-amd64/cruisemic cmd/cruisemic/main.go || exit 1
+zip -q -r cruisemic.${VERSION}.darwin-amd64.zip cruisemic.${VERSION}.darwin-amd64 || exit 1
+zip -q -r cruisemic.${VERSION}.linux-amd64.zip cruisemic.${VERSION}.linux-amd64 || exit 1
