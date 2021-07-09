8b378ece14291c242ec53bf746cff1bd0589143b
Chris Berthiaume <chrisbee@uw.edu>
2021-07-09 11:37:12 -0700

    Put a v in front of version

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ VERSION=$(git describe --tags)
 [[ -d build ]] || mkdir build
 GOOS=darwin GOARCH=amd64 go build -o build/cruisemic.${VERSION}.darwin-amd64 cmd/cruisemic/main.go || exit 1
 GOOS=linux GOARCH=amd64 go build -o build/cruisemic.${VERSION}.linux-amd64 cmd/cruisemic/main.go || exit 1
+openssl dgst -sha256 build/*.${VERSION}.* | sed -e 's|build/||g'
