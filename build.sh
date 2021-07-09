e90babd90b46ced8357a0c0627befe10020ce664
Chris Berthiaume <chrisbee@uw.edu>
2021-07-09 11:29:48 -0700

    Change build process

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Build cruisemic command-line tool for 64-bit MacOS and Linux
 
-VERSION=$(git describe --long --dirty)
+VERSION=$(git describe --tags)
 
-[[ -d cruisemic.darwin-amd64 ]] && rm -rf cruisemic.darwin-amd64
-[[ -d cruisemic.linux-amd64 ]] && rm -rf cruisemic.linux-amd64
-GOOS=darwin GOARCH=amd64 go build -o cruisemic.${VERSION}.darwin-amd64/cruisemic cmd/cruisemic/main.go || exit 1
-GOOS=linux GOARCH=amd64 go build -o cruisemic.${VERSION}.linux-amd64/cruisemic cmd/cruisemic/main.go || exit 1
-zip -q -r cruisemic.${VERSION}.darwin-amd64.zip cruisemic.${VERSION}.darwin-amd64 || exit 1
-zip -q -r cruisemic.${VERSION}.linux-amd64.zip cruisemic.${VERSION}.linux-amd64 || exit 1
+[[ -d build ]] || mkdir build
+GOOS=darwin GOARCH=amd64 go build -o build/cruisemic.${VERSION}.darwin-amd64 cmd/cruisemic/main.go || exit 1
+GOOS=linux GOARCH=amd64 go build -o build/cruisemic.${VERSION}.linux-amd64 cmd/cruisemic/main.go || exit 1
--- cmd/cruisemic/main.go
+++ cmd/cruisemic/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/ctberthiaume/cruisemic/storage"
 )
 
-var version = "0.2.2"
+var version = "0.2.3"
 
 var nameFlag = flag.String("name", "", "Cruise or experiment name (required)")
 var rawFlag = flag.Bool("raw", false, "Don't filter for whitelisted ASCII characters: Space to ~, TAB, LF, CR")
