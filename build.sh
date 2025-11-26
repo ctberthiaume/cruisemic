dcbc7e4d24cf08ff36597239e69cb72ac63fd029
Chris T. Berthiaume <chrisbee@uw.edu>
2025-11-26 14:35:07 -0800

    Add Apple Silicon build target

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ VERSION=$(git describe --tags)
 [[ -d build ]] || mkdir build
 GOOS=darwin GOARCH=amd64 go build -o build/cruisemic.${VERSION}.darwin-amd64 cmd/cruisemic/main.go || exit 1
 GOOS=linux GOARCH=amd64 go build -o build/cruisemic.${VERSION}.linux-amd64 cmd/cruisemic/main.go || exit 1
+GOOS=darwin GOARCH=arm64 go build -o build/cruisemic.${VERSION}.darwin-arm64 cmd/cruisemic/main.go || exit 1
 openssl dgst -sha256 build/*.${VERSION}.* | sed -e 's|build/||g'
