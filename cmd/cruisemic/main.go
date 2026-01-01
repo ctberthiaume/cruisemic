a66e5801e3997007e613fea3589dfc736cdb5956
Chris T. Berthiaume <chrisbee@uw.edu>
2026-01-01 11:25:10 -0800

    Exit if -dir not provided

--- cmd/cruisemic/main.go
+++ cmd/cruisemic/main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/ctberthiaume/cruisemic/storage"
 )
 
-var version = "v0.9.2"
+var version = "v0.9.3"
 
 var nameFlag = flag.String("name", "", "Cruise or experiment name (required)")
 var noCleanFlag = flag.Bool("noclean", false, "Don't filter for whitelisted ASCII characters: Space to ~, TAB, LF, CR")
@@ -57,6 +57,7 @@ func main() {
 	}
 	if *dirFlag == "" {
 		fmt.Println("-dir is required")
+		os.Exit(1)
 	}
 	if *wrappedFlag && *udpFlag {
 		fmt.Println("-wrapped and -udp cannot both be set")
